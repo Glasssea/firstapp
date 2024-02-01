5fcbc6bf5553c2220431902b0b6e8011c837e6ca
dbsdbs17@gmail.com <dbsdbs17@gmail.com>
2024-02-01 14:24:20 +0900

    number.php만들었음

---
+++ number.php
@@ -0,0 +1,24 @@
+<link rel="stylesheet" type="text/css" href="number.css">
+<div class="wall_5">5층
+    <div class="hollpaper_5">찢어진벽지
+        <div class="holl_5">구멍</div>
+    </div>
+    <div class="water_5">water</div>
+    <div class="board_5"></div>
+</div>
+
+
+<div class="wall_4">
+    <div class="holl_4"></div>
+    <div class="board_4"></div>
+    <div class="board_41"></div>
+</div>
+
+<div class="wall_3">3층
+    <div class="bluewall_3"></div>
+    <div class="board_3"></div>
+    <div class="board_31"></div>
+</div>
+
+<!-- 1200*2400 3개 -->
+<!-- 1200*1570 1개 -->
